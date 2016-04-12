92a3a3188eb2aa0762db525fc9f82de6c82c8d8b
Zyrian Gantuangco <zslice0915@hotmail.com>
2016-04-12 18:01:01 -0400

    Removed unnecessary code

--- Music/Controllers/GenreController.cs
+++ Music/Controllers/GenreController.cs
@@ -75,67 +75,6 @@ namespace Music.Controllers
             return View(genre);
         }
 
-        // GET: Albums/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Album album = db.Albums.Find(id);
-            if (album == null)
-            {
-                return HttpNotFound();
-            }
-            ViewBag.ArtistID = new SelectList(db.Artists, "ArtistID", "Name", album.ArtistID);
-            ViewBag.GenreID = new SelectList(db.Genres, "GenreID", "Name", album.GenreID);
-            return View(album);
-        }
-
-        // POST: Albums/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "AlbumID,Title,GenreID,Price,ArtistID")] Album album)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(album).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            ViewBag.ArtistID = new SelectList(db.Artists, "ArtistID", "Name", album.ArtistID);
-            ViewBag.GenreID = new SelectList(db.Genres, "GenreID", "Name", album.GenreID);
-            return View(album);
-        }
-
-        // GET: Albums/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Album album = db.Albums.Find(id);
-            if (album == null)
-            {
-                return HttpNotFound();
-            }
-            return View(album);
-        }
-
-        // POST: Albums/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            Album album = db.Albums.Find(id);
-            db.Albums.Remove(album);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
-
         protected override void Dispose(bool disposing)
         {
             if (disposing)
