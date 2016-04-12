96dc1e66407ae8819b3b69b9be164648570b166b
Zyrian Gantuangco <zslice0915@hotmail.com>
2016-04-12 18:04:50 -0400

    Undo previous change

--- Music/Models/Genre.cs
+++ Music/Models/Genre.cs
@@ -8,9 +8,8 @@ namespace Music.Models
 {
     public class Genre
     {
-        [Required(ErrorMessage = "Genre is required")]
-        [MaxLength(20)]
         public int GenreID { get; set; }
+        [Required(ErrorMessage = "Genre is required")][MaxLength(20)]
         public string Name { get; set; }
         public List<Album> Albums { get; set; }
     }
