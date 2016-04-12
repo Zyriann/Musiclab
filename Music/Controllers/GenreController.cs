5b5a6292300b22faa575a4b47cd603792067b041
Zyrian Gantuangco <zslice0915@hotmail.com>
2016-04-12 15:52:08 -0400

    First Commit

---
+++ Music/Controllers/GenreController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Music.Models;
+
+namespace Music.Controllers
+{
+    public class GenreController : Controller
+    {
+        private MusicContext db = new MusicContext();
+
+        // GET: Genre
+        public ActionResult Index()
+        {
+            var genre = db.Genres.Include(a => a.Name);
+            return View(genre.ToList());
+        }
+
+        public ActionResult ShowGenre(int id)
+        {
+            var genre = db.Genres
+                .Include(a => a.Albums)
+                .Include(a => a.Name)
+                .Where(a => a.GenreID == id);
+            return View(genre.ToList());
+        }
+
+        public ActionResult Create()
+        {
+            ViewBag.GenreID = new SelectList(db.Genres.OrderByDescending(g => g.Name), "GenreID", "Name");
+            return View();
+        }
+    }
+}
---
+++ Music/Models/Genre.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Music.Models
+{
+    public class Genre
+    {
+        public int GenreID { get; set; }
+        public string Name { get; set; }
+        public List<Album> Albums { get; set; }
+    }
+}
