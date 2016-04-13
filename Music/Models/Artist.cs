d9845c4033eea7b4ed03edfac248dba2908c51b2
Zyrian Gantuangco <zslice0915@hotmail.com>
2016-04-12 21:03:17 -0400

    Required

--- Music/Models/Artist.cs
+++ Music/Models/Artist.cs
@@ -1,8 +1,16 @@
-﻿namespace Music.Models
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Music.Models
 {
     public class Artist
     {
         public int ArtistID { get; set; }
+
+        [Required(ErrorMessage = "Artist Name is Required")]
         public string Name { get; set; }
     }
 }
