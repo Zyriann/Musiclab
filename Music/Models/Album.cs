ad7bf19f2ec58cb69ff4f0670c8dca966174e411
Zyrian Gantuangco <zslice0915@hotmail.com>
2016-04-12 21:03:30 -0400

    Alignment

--- Music/Models/Album.cs
+++ Music/Models/Album.cs
@@ -10,13 +10,18 @@ namespace Music.Models
     {
 
         public int AlbumID { get; set; }
+
         [Required(ErrorMessage ="Album title is required")]
         public string Title { get; set; }
+
         [Display(Name ="Genre")]
         public int GenreID { get; set; }
         public Genre Genre { get; set; }
-        [Required(ErrorMessage = "Price is required")][Range(0.01, 100.0)]
+
+        [Required(ErrorMessage = "Price is required")]
+        [Range(0.01, 100.0)]
         public decimal Price { get; set; }
+
         [Display(Name = "Artist")]
         public int ArtistID { get; set; }
         public Artist Artist { get; set; }
